e09be96fffde93843f178083450c6d1367d16b67
jimba tshetrim <jtshetrim@gmail.com>
2023-04-12 23:28:48 +0600

    feat: added skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HeaderComponent,
     BrowserModule
   ],
   providers: [],
