0d5656d273e35b2da951ea96abf895150f1ee944
jimba tshetrim <jtshetrim@gmail.com>
2023-04-12 23:30:01 +0600

    feat: generated main container

---
+++ src/app/main-container/main-container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-container.component.html',
+  styleUrls: ['./main-container.component.css']
+})
+export class MainContainerComponent {
+
+}
