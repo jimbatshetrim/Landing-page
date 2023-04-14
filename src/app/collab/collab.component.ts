c813287185d5726564d9d92afd5dcb423252c77e
jimba tshetrim <jtshetrim@gmail.com>
2023-04-14 23:10:40 +0600

    feat: collab block generate

---
+++ src/app/collab/collab.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-collab',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './collab.component.html',
+  styleUrls: ['./collab.component.css']
+})
+export class CollabComponent {
+
+}
