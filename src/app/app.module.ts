cb0a5afa4594b8f46af50366b945ad5fdef6812d
jimba tshetrim <jtshetrim@gmail.com>
2023-04-14 23:01:29 +0600

    feat: company info block upated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompanyWorkComponent } from './company-work/company-work.component';
 import { ContainerBoxComponent } from './container-box/container-box.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,6 +17,7 @@ import { MainContainerComponent } from './main-container/main-container.componen
     MainContainerComponent,
     ContainerBoxComponent,
     FooterComponent,
+    CompanyWorkComponent,
     BrowserModule
   ],
   providers: [],
